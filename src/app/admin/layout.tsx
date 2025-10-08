cdb081e90e121493f84297520ad18709003d11e8
PUSHP RAJ SHARMA <kamalabai181@gmail.com>
2025-10-08 15:46:47 +0000

    Fix some Problems: - In mobile screen the navbar is not opening fix this

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -17,7 +17,7 @@ const navItems = [
 ];
 
 const DesktopSidebar = ({ pathname }: { pathname: string }) => (
-  <div className="hidden border-r bg-muted/40 md:block">
+  <div className="hidden border-r bg-background md:block">
     <div className="flex h-full max-h-screen flex-col gap-2">
       <div className="flex h-16 items-center border-b px-4 lg:px-6">
         <Link href="/" className="flex items-center gap-2 font-semibold">
